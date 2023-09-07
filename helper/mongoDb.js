f4b9ffaeeb78d7e28f59816cffb40eb5726b4c75
Bibek Regmi <bibek.regmi@swifttech.com>
2023-09-07 18:01:59 +0545

    tried connecting mongoDb will try later

---
+++ helper/mongoDb.js
@@ -0,0 +1,17 @@
+// "use strict";
+// const mongoose = require('mongoose')
+// (()=>{
+    
+//     module.exports={
+        
+//         const db = {};
+//         db.mongoose = mongoose;
+//         db.url = dbConfig.url;
+//         db.students = require("./students")(mongoose);
+        
+//         module.exports = db;
+//     }
+//     module.exports= mongoose.connect
+
+
+// })()
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const dotenv= require('dotenv')
+const grpc = require('@grpc/grpc-js')
 dotenv.config()
-const bodyParser=require(body-parser)
 app=  express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-const port = process.env.PORT
-app.listen(port,()=>{
-    console.log(`Server connected to port ${port}`);
-})
+
+const server= new grpc.Server()
+// app.use(bodyParser.urlencoded({extended: true}));
+// const port = process.env.PORT
+// app.listen(port,()=>{
+//     console.log(`Server listening to port ${port}`);
+// })
